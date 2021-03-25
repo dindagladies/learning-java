ab14fe78c800b398734d799c973228af16a88350
dindagladies <48267244+dindagladies@users.noreply.github.com>
2021-03-25 21:49:29 +0700

    add project

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+public class HelloWorld{
+
+    // like index
+    public static void main(String[] args){
+
+        // membuat variabel
+        String nama, alamat;
+        int usia;
+        double tinggi;
+
+        // mengisi varibel
+        nama = "Petani Kode";
+        alamat = "Internet"
+        usia = 20;
+        tinggi = 164.6;
+
+        // mencetak ke layar isi variabel
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Usia: " + usia);
+        System.out.println("Tinggi: " + tinggi + "cm");
+    }
+}
+// https://www.malasngoding.com/cara-membuat-crud-dengan-java-mysql-part-1/
